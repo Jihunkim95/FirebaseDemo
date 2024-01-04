b894f41cccd11a88ee1a9d06b09b23b7445943eb
Jihunkim95 <darak551@naver.com>
2024-01-04 10:47:10 +0900

    init: 초기설정

---
+++ FirebaseDemo/FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by 김지훈 on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
